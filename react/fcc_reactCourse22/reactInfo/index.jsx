7ed2b22df9be85338aa2293753a8dafe77361e85
Ploppa-Smurph <jeffreywalley@student.bpcc.edu>
2022-06-01 18:03:21 -0500

    started Scrimba / fCC React 2022 course

---
+++ react/fcc_reactCourse22/reactInfo/index.jsx
@@ -0,0 +1,2 @@
+// using ReactDom to 'render' the component to the DOM -- it is placing this in the HTML 'id="root" / or #root'
+ReactDOM.render(<h1>This is my reactive statement</h1>, document.getElementById("root"));
