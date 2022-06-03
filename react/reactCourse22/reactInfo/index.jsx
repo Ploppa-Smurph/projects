fc80d4c75e2f1bd3a104dad53cf1555903233271
Ploppa-Smurph <jeffreywalley@student.bpcc.edu>
2022-06-03 07:47:51 -0500

    more React watched, start of static webpage project (project 01)

--- react/reactCourse22/reactInfo/index.jsx
+++ react/reactCourse22/reactInfo/index.jsx
@@ -1,18 +1,18 @@
 
-
-const page = (
-    <div>
-        <h1>react project starter</h1>
-        <p>this is a test</p>
-        <ol>
-            <li>react is composable</li>
-            <li>react is declarative</li>
-            <li>react is a hireable skill</li>
-            <li>react is actively maintained</li>
-        </ol>
-    </div>
+const nav = (
+    <nav>
+    <a href="https://flyclipart.com/react-logo-import-io-react-logo-png-221727" title="React Logo Import Io - React Logo PNG"><img src="https://flyclipart.com/thumb2/react-logo-import-io-221727.png" width="350" /></a>
+        <h1>Shrimp & Snail Friends</h1>
+        <p>your place for shrimp & snails friends</p>
+        <ul>
+            <li>About</li>
+            <li>Blog</li>
+            <li>Merchandise</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
 )
+//-- creates JSON objects -- not working HTML
+//document.getElementById("root").append(JSON.stringify(page)) 
 
-document.getElementById("root").append(JSON.stringify(page))
-
-//ReactDOM.render(page, document.getElementById("root"))
+ReactDOM.render(nav, document.getElementById("root"))
