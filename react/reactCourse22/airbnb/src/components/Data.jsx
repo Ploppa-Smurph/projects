f33a67c1ec76798e4bd2088ba576be346ea24234
Ploppa-Smurph <jeffreywalley@student.bpcc.edu>
2022-06-07 19:01:09 -0500

    worked more on Walley Works React site

--- react/reactCourse22/airbnb/src/components/Data.jsx
+++ react/reactCourse22/airbnb/src/components/Data.jsx
@@ -3,8 +3,16 @@ import dataLogo from "../img/wwLogo.png";
 
 export default function Data() {
     return (
-        <div className="data-container">
-        <img src={dataLogo} alt="Data" className="data-img" />
+    <div className="data-card">
+            <img src={dataLogo} alt="Data" className="data-img" />
+        <div className="data-stats">
+            <i class="fa-solid fa-star"></i>
+            <span>5.0</span>
+            <span className="text-muted">(6) </span>
+            <span className="text-muted">USA</span>
         </div>
+            <p>life lessons with Ra</p>
+            <p><span className="bold">From $125</span> / person</p>
+    </div>
     )
 }
