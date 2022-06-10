f11581e1e54c214698f89e4cab8adcad0e76e48d
Ploppa-Smurph <jeffreywalley@student.bpcc.edu>
2022-06-10 18:46:25 -0500

    worked on more props in React proj 3c

--- react/reactCourse22/walleyworks2/src/components/App.jsx
+++ react/reactCourse22/walleyworks2/src/components/App.jsx
@@ -10,15 +10,34 @@ export default function App() {
       <Header />
       <MainContent />
       <Joke question="Why don’t pirates take a shower before they walk the plank?"
-            answer="They just wash up on shore."/>
+            answer="They just wash up on shore."
+            isBarJoke={false} 
+            upvotes={409}
+            downvotes={2}
+            comments={[{author: "", title: "", body: "", time: ""}, {author: "", title: "", body: "", time: ""}]}/>
       <Joke question="A grasshopper walks into a bar. The bartender looks at him and says, “Hey, they named a drink after you!”"
-            answer="“Really?” replies the grasshopper. “There’s a drink named Stan?”"/>
+            answer="“Really?” replies the grasshopper. “There’s a drink named Stan?”"
+            isBarJoke={true} 
+            upvotes={436}
+            downvotes={12}
+            comments={[{author: "", title: "", body: "", time: ""}, {author: "", title: "", body: "", time: ""}]}/>
       <Joke question="A man went into a seafood restaurant and asked for a lobster tail. The waitress smiled sweetly and said, “Once upon a time there was this handsome lobster…”"
-            answer=""/>
+            isBarJoke={false} 
+            upvotes={587}
+            downvotes={146}
+            comments={[{author: "", title: "", body: "", time: ""}, {author: "", title: "", body: "", time: ""}]}/>
       <Joke question="Two eggs, a bagel, and a sausage walk into a bar. “Bartender, my friends and I would like a cold one,” says one of the eggs."
-            answer="“Sorry,” the barman replies. “We don’t serve breakfast.”"/>
+            answer="“Sorry,” the barman replies. “We don’t serve breakfast.”"
+            isBarJoke={true}
+            upvotes={241}
+            downvotes={1}
+            comments={[{author: "", title: "", body: "", time: ""}, {author: "", title: "", body: "", time: ""}]}/>
       <Joke question="At our weekly alumni meetings, the football coach shows the film of the most recent game and holds a question-and-answer period afterward. One of the alumni, who had played on the football team many years ago and had a son on this year’s squad, posed a question concerning the defensive line. “I’d like to know,” he said, “why our boys are so slow getting into the opposition’s backfield after the ball is snapped.”"
-            answer="“I’m not sure, Fred,” answered the coach. “But it could be hereditary.”"/>
+            answer="“I’m not sure, Fred,” answered the coach. “But it could be hereditary.”"
+            isBarJoke={false} 
+            upvotes={603}
+            downvotes={85}
+            comments={[{author: "", title: "", body: "", time: ""}, {author: "", title: "", body: "", time: ""}]}/>
       <Footer />
     </div>
   )
--- react/reactCourse22/walleyworks2/src/components/Joke.jsx
+++ react/reactCourse22/walleyworks2/src/components/Joke.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
 export default function Joke(props) {
+    console.log(props.isBarJoke)
+    console.log(props.upvotes)
+    console.log(props.downvotes)
+    console.log(props.comments)
     return (
         <div className="joke-container">
-            <div class="joke">
+            <div className="joke">
                 {/* if 'props.question' is a 'truthy value' (true) - then render the content to the right */}
                 {props.question && <h3>{props.question}</h3>}
                 {/* if 'props.answer' is a 'truthy value' (true) - then render the content to the right */}
