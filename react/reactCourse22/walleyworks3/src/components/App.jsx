6064399484e715bfcfb714b189a338cb2988c613
Ploppa-Smurph <jeffreywalley@student.bpcc.edu>
2022-06-13 11:18:06 -0500

    got web images (picsum) to work in the project

--- react/reactCourse22/walleyworks3/src/components/App.jsx
+++ react/reactCourse22/walleyworks3/src/components/App.jsx
@@ -5,7 +5,7 @@ import Hero from './Hero.jsx'
 import MainContent from './MainContent.jsx'
 import Card from './Card.jsx'
 import Footer from './Footer.jsx'
-import dataLogo from "../img/wwCirLogo.png";
+/* import dataLogo from "../img/wwCirLogo.png; */
 
 export default function App() {
     return(
@@ -13,28 +13,28 @@ export default function App() {
         <Header />
         <Hero />
         <MainContent />
-        <Card img={dataLogo}
+        <Card img="https://picsum.photos/id/124/250/400"
               rating={4.8}
               reviewCount={61}
               country={'US'}
-              title={'Felting Lessons with Ra'}
+              title={'Fishing Lessons with Ra'}
               price={125}
         />
-        <Card img={dataLogo}
+        <Card img="https://picsum.photos/id/23/250/400"
               rating={3.8}
               reviewCount={1861}
               country={'US'}
               title={'Learning the Shiv'}
               price={14.50}
         />
-        <Card img={dataLogo}
+        <Card img="https://picsum.photos/id/590/250/400"
               rating={5.0}
               reviewCount={60956}
               country={'France'}
               title={'A Hug from Grand-Mere'}
               price={750}
         />
-        <Card img={dataLogo}
+        <Card img="https://picsum.photos/id/284/250/400"
               rating={0.76}
               reviewCount={904}
               country={'US'}
--- react/reactCourse22/walleyworks3/src/components/Card.jsx
+++ react/reactCourse22/walleyworks3/src/components/Card.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 export default function Card(props) {
+    //console.log(props)
     return (
     <div className="data-card">
-            <img src={props.img} alt="Data" className="data-img" />
-        <div className="data-stats">
+            <img src= {props.img} alt="Images of Fun Things" className="data-img" />
+            <div className="data-stats">
             <i class="fa-solid fa-star"></i>
             <span>{props.rating}&nbsp;</span>
             <span className="text-muted">({props.reviewCount})&nbsp;</span>
