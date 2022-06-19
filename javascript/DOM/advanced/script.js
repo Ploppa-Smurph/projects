402b0c581e65b36c56ed7102a81a218bdc51e072
Ploppa-Smurph <jeffreywalley@student.bpcc.edu>
2022-06-19 14:04:23 -0500

    more of the javascript DOM drum project --event listeners

--- javascript/DOM/advanced/script.js
+++ javascript/DOM/advanced/script.js
@@ -1,2 +1,10 @@
 document.querySelector("#notesTitle").textContent = "WALLEY WORKS and London App Brewery Present: NOTES"
-document.querySelector("#projectTitle").innerHTML = "<em>DRUM PROJECT</em>"
+document.querySelector("#project-header-title").innerHTML = "<em>DRUM PROJECT</em>"
+
+// eventlistener --
+ 
+document.querySelector("button").addEventListener("click", handleClick)
+function handleClick() { 
+    alert("I got clicked")
+}  
+
