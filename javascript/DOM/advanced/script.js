de08881b4b93a856d38754602d82af5c44d863a5
Ploppa-Smurph <jeffreywalley@student.bpcc.edu>
2022-06-20 11:01:03 -0500

    more complete on javascript eventlisteners

--- javascript/DOM/advanced/script.js
+++ javascript/DOM/advanced/script.js
@@ -3,8 +3,20 @@ document.querySelector("#project-header-title").innerHTML = "<em>DRUM PROJECT</e
 
 // eventlistener --
  
-document.querySelector("button").addEventListener("click", handleClick)
+/* document.querySelector("button").addEventListener("click", handleClick)
 function handleClick() { 
     alert("I got clicked")
 }  
+ */
 
+// the above listener function written as an anonymous function -- shortened even further into an anonymous arrow function
+// document.querySelector("button").addEventListener("click", () => alert("I got clicked"));
+
+/* Event Listener for ALL of the buttons using a 'for' loop */
+//determine the amount of "drum buttons" in the HTML
+let drumButtonNum = document.querySelectorAll(".drum").length; // variable to 'get' length
+for (var i=0; i<drumButtonNum; i++) {   // iterate through the .drum buttons
+document.querySelectorAll(".drum")[i].addEventListener("click", () => alert("I got clicked")); // anonymous arrow function w/ event listener
+}
+
+// ep03 higher order functions and passing functions
