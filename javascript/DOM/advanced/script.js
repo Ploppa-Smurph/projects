cc5fb6cc735631f920fb429724e4814e7434504c
Ploppa-Smurph <jeffreywalley@student.bpcc.edu>
2022-06-23 19:07:03 -0500

    more styling and scripting complete from the Drum Kit Project

--- javascript/DOM/advanced/script.js
+++ javascript/DOM/advanced/script.js
@@ -21,12 +21,54 @@ for (var i=0; i<drumButtonNum; i++) {    // iterate through the .drum buttons
  */
 // ep03 higher order functions and passing functions
 // find which button triggered the event so that each can call a different sound    
-document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+/* document.querySelectorAll(".drum")[i].addEventListener("click", function() {
         console.log(this.innerHTML)
 }); 
-}
+} */
 
 /* let drum = new Audio('./mp3s/tom_plastic_hard.mp3');
         drum.play() */
 
-// ep06 Switch functions in javascript
+// ep06 Switch functions in javascript //
+document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    let buttonInnerHTML = this.innerHTML;
+
+    switch (buttonInnerHTML) {
+        case "W":
+            let tom_hard = new Audio('./mp3s/tom_plastic_hard.mp3');
+            tom_hard.play() 
+        break;
+        case "A":
+            let tom_mid = new Audio('./mp3s/tom_mid_low_01.mp3');
+            tom_mid.play()
+        break; 
+        case "S":
+            let tom_high = new Audio('./mp3s/tom_high_01.mp3');
+            tom_high.play() 
+        break;
+        case "D":
+            let kick_drum = new Audio('./mp3s/kickDrum_01.mp3');
+            kick_drum.play() 
+        break;
+        case "J":
+            let hi_closed = new Audio('./mp3s/gamelan_drum_large.mp3');
+            hi_closed.play() 
+        break;
+        case "K":
+            let hi_open = new Audio('./mp3s/beatbox_open_hi_hat.mp3');
+            hi_open.play() 
+        break;
+        case "L":
+            let cymbal_crash = new Audio('./mp3s/cymbal_crash.mp3');
+            cymbal_crash.play() 
+        break;   
+        default: 
+            console.log(buttonInnerHTML)
+            break;
+    }
+}); 
+}
+
+// Ep08 Using Keyboard Event Listeners //
+
+
