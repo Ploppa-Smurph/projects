5f95921f7adbf0b3eb5392398896ea4fb3d5af98
Ploppa-Smurph <jeffreywalley@student.bpcc.edu>
2022-06-26 16:32:58 -0500

    some styling on walleyworks3 done

--- react/reactCourse22/walleyworks3/src/components/Header.jsx
+++ react/reactCourse22/walleyworks3/src/components/Header.jsx
@@ -1,13 +1,12 @@
 // must import React in order for JSX to work
 import React from 'react';
-import wwLogo from "../img/wwCat_nobg.png";
 
 export default function Header() {
     return (
     <header className="header">
     
     <div className="logo-container">
-    <img src={wwLogo} alt="Walley Works Logo" className="nav-logo" />
+    <img src={"./img/wwCat_nobg.png"} alt="Walley Works Logo" className="nav-logo" />
   </div>
         {/* <div className="nav-title">
         <h1>WALLEY WORKS</h1>
