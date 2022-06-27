94378aac87153416482e0a51dec550ec24de82e3
Ploppa-Smurph <jeffreywalley@student.bpcc.edu>
2022-06-27 18:39:55 -0500

    setting up wwtour travel site

---
+++ react/reactCourse22/wwtour/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <header className="header">
+            <div className="logo-container">
+                <img className="nav-logo" src={"./img/wwTour_rgb(32, 113, 120).png"} alt="wwTourLogo" />    
+            </div>
+        </header>
+    )
+}
