f88b2f5e8258ccf6252be17d0816db70db2bf40f
Ploppa-Smurph <jeffreywalley@student.bpcc.edu>
2022-06-29 22:10:36 -0500

    styled some on meme site

--- react/reactCourse22/meme/src/components/Footer.jsx
+++ react/reactCourse22/meme/src/components/Footer.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 export default function Footer() {
     return (
         <footer className="footer">
-            <h4>FOOTER</h4>
+            <img className="header-logo" src={"./img/meme_dark.png"} alt="wwTourLogo" />
+            <p className="indicia">&copy; 2022 WALLEY WORKS. All rights reserved.</p>    
         </footer>
     )
 }
--- react/reactCourse22/meme/src/components/Header.jsx
+++ react/reactCourse22/meme/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Header() {
     return (
         <header className="header">
-            <h3>Header</h3>
+            <img className="header-logo" src={"./img/meme_dark_noBG.png"} alt="wwTourLogo" />
         </header>
     )
 } 
