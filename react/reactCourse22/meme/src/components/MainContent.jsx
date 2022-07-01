bd7e1ac29ac4b8dfcacb2618d4d8b22986b4a5ed
Ploppa-Smurph <jeffreywalley@student.bpcc.edu>
2022-07-01 13:06:07 -0500

    layout of meme project

--- react/reactCourse22/meme/src/components/MainContent.jsx
+++ react/reactCourse22/meme/src/components/MainContent.jsx
@@ -5,9 +5,9 @@ export default function MainContent() {
         <div className="maincontent-container">
             <h1>Walley Works/Bob Ziroll: the Meme Slinger</h1>
             <form className="meme-form">
-            <input className="meme-text" type="text" placeholder="Top Text" />
-            <input className="meme-text" type="text" placeholder="Bottom Text" />
-            <button className="meme-button">Sling NEW Meme Image <i class="fa-solid fa-image"></i></button>
+            <input className="meme-text" type="text" placeholder="Enter Top Text" />
+            <input className="meme-text" type="text" placeholder="Enter Bottom Text" />
+            <button className="meme-button">Sling NEW Meme Image<i class="fa-solid fa-image"></i></button>
             </form>
             <img className="meme-image" src={"#"} alt="Meme Template" />            
         </div>
