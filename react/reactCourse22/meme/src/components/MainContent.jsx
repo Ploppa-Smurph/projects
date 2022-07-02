9244b5553c35f2ba529ce6973774ba27eca03b98
Ploppa-Smurph <jeffreywalley@student.bpcc.edu>
2022-07-01 19:38:07 -0500

    event listeners in React

--- react/reactCourse22/meme/src/components/MainContent.jsx
+++ react/reactCourse22/meme/src/components/MainContent.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import memesData from "../memesData.js"
 
 export default function MainContent() {
+
+    function handleClick() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        console.log(url)
+    }
+
     return (
         <div className="maincontent-container">
             <h1>Walley Works/Bob Ziroll: the Meme Slinger</h1>
-            <form className="meme-form">
+            <div className="meme-form">
             <input className="meme-text" type="text" placeholder="Enter Top Text" />
             <input className="meme-text" type="text" placeholder="Enter Bottom Text" />
-            <button className="meme-button">Sling NEW Meme Image<i class="fa-solid fa-image"></i></button>
-            </form>
+            <button onClick={handleClick} className="meme-button">Sling NEW Meme Image<i className="fa-solid fa-image"></i></button>
+            </div>
             <img className="meme-image" src={"#"} alt="Meme Template" />            
         </div>
     )
