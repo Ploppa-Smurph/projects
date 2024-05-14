4e2acd8f83d715bf847ff6d7637b74b5d588e46c
Ploppa-Smurph <jeffreywalley@student.bpcc.edu>
2024-05-14 13:17:28 -0500

    work on shrimp site

--- websites/shrimp/script/script.js
+++ websites/shrimp/script/script.js
@@ -1,5 +1,6 @@
 var navLinks = document.getElementById("navLinks");
 
+
 function showMenu() {
     navLinks.style.right = "0";
 }
@@ -7,3 +8,20 @@ function showMenu() {
 function hideMenu() {
     navLinks.style.right = "-200px";
 }
+
+/* read more / less button (in Blog section) */
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+    
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
